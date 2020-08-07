c19be4f48d3bbc00b4913a8e14bb9253832dfaa1
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-08-07 16:20:26 -0500

    Update button_component_primary.blade.php

--- resources/views/components/button_component_primary.blade.php
+++ resources/views/components/button_component_primary.blade.php
@@ -1,5 +1,5 @@
 <span class="inline-flex rounded-md shadow-sm">
-  <{{ $tag }} @if (!is_null($onclick)) onclick="{{ $onclick }}" @endif @if (!is_null($href)) href="{{$href}}" @endif class="inline-flex items-center px-{{ $measurements['px'] }} py-{{ $measurements['py'] }} border border-transparent text-xs leading-{{ $measurements['leading'] }} font-medium rounded text-white bg-{{ $color }}-600 hover:bg-{{ $color }}-500 focus:outline-none focus:border-{{ $color }}-700 focus:shadow-outline-{{ $color }} active:bg-{{ $color }}-700 transition ease-in-out duration-150 {{ $classes }} {{$attributes}}">
+  <{{ $tag }} @include('bcl::components.partials.livewire-inputs') @if (!is_null($onclick)) onclick="{{ $onclick }}" @endif @if (!is_null($href)) href="{{$href}}" @endif class="inline-flex items-center px-{{ $measurements['px'] }} py-{{ $measurements['py'] }} border border-transparent text-xs leading-{{ $measurements['leading'] }} font-medium rounded text-white bg-{{ $color }}-600 hover:bg-{{ $color }}-500 focus:outline-none focus:border-{{ $color }}-700 focus:shadow-outline-{{ $color }} active:bg-{{ $color }}-700 transition ease-in-out duration-150 {{ $classes }} {{$attributes}}">
     {{ $slot }}
   </{{ $tag }}>
 </span>
