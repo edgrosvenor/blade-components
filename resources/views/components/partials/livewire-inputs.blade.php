652d1698b8e4e33ad00387d3272cad201d112c5a
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-08-07 16:16:19 -0500

    Update livewire-inputs.blade.php

--- resources/views/components/partials/livewire-inputs.blade.php
+++ resources/views/components/partials/livewire-inputs.blade.php
@@ -1,2 +1,3 @@
 @if ($attributes->offsetExists('wire:model')) wire:model.debounce.{{ $attributes->get('debounce') ?? '150ms' }}="{{ $attributes->get('wire:model') }}" wire:key="{{ str_replace('.', '_', $attributes->get('wire:model')) }}" @endif
 @if ($attributes->offsetExists('wire:model.lazy')) wire:model.lazy="{{ $attributes->get('wire:model.lazy') }}" wire:key="{{ str_replace('.', '_', $attributes->get('wire:model.lazy')) }}" @endif
+@if ($attributes->offsetExists('wire:click')) wire:click="{{ $attributes->get('wire:click') }}" @endif
