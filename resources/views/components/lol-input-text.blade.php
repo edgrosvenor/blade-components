502182d7eb8da544a3b0696f80bc5718a77d3120
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-08-17 16:02:39 -0500

    Update lol-input-text.blade.php

--- resources/views/components/lol-input-text.blade.php
+++ resources/views/components/lol-input-text.blade.php
@@ -1,6 +1,6 @@
 <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:py-5">
     <label for="$attributes->get('id') ?? 'x'.md5(json_encode($attributes)) " class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-        {{ $attributes->get('label') ?? Str::rand(8) }}
+        {{ $attributes->get('label') ?? Str::random(8) }}
     </label>
     <div class="mt-1 sm:mt-0 sm:col-span-2">
         <div class="max-w-lg flex rounded-md shadow-sm">
